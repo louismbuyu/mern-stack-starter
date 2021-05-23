d5f14a3e2a7f74b74a8fc46b56dfa8ab5654de4e
louismbuyu <louisnelsonm@gmail.com>
2021-05-23 01:33:19 -0500

    pass in user id when creating a post

--- src/domains/posts/post.service.ts
+++ src/domains/posts/post.service.ts
@@ -6,7 +6,7 @@ import {UserCollection} from '../users/user.schema';
 
 export const createPost = async (userId: string, postRequest: PostRequest, session: ClientSession): Promise<Post> => {
 	const partialPostSchema = convertPostRequestToPostSchema(postRequest);
-	const savedPostSchema = await PostCollection.insertMany([partialPostSchema], { session });
+	const savedPostSchema = await PostCollection.insertMany([{...partialPostSchema, user: userId }], { session });
 	await UserCollection.findOneAndUpdate({_id: userId },
 		{ $inc: { postsCount: 1 },  },
 		{ runValidators: true, new: true })
